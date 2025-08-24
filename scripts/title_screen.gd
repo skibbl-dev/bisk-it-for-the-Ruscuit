45d986183a21dddc911774b622e23ff603b1c479
sᴋɪʙʙʟ_ <62254455+skibbl-dev@users.noreply.github.com>
2025-08-24 15:19:23 -0600

    mista green song 0.1

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,10 +1,10 @@
 extends Control
 
-const ANOTHER_TIME_PERHAPS = preload("res://assets/music/Another Time Perhaps.mp3")
+const MISTA_GREEN_ROUGH_LOOP = preload("res://assets/music/mista green rough loop.wav")
 
 @onready var conducted_player: ConductedAnimationPlayer = $ConductedAnimationPlayer
 
 func _ready() -> void:
-	Conductor.set_song(ANOTHER_TIME_PERHAPS, 187)
+	Conductor.set_song(MISTA_GREEN_ROUGH_LOOP, 81)#162)
 	Conductor.play()
 	conducted_player.play("pulsing")
