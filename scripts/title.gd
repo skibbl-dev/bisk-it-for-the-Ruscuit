30319d6052c536fc6ab3444aa4427a3b414ce33d
dibin_17 <dylan@rut66.net>
2025-08-26 16:51:16 -0600

    title screen goated

--- scripts/title.gd
+++ scripts/title.gd
@@ -5,22 +5,42 @@ const MISTA_GREEN_ROUGH_LOOP = preload("res://assets/music/mista green rough loo
 
 @onready var conducted_player: ConductedAnimationPlayer = $ConductedAnimationPlayer
 
-@onready var start: Area2D = $START
+@onready var states = [
+	$START/dam0,
+	$START/dam1,
+	$START/dam2,
+	$START/dam3
+]
 
-var count := 0
+var current_state := 0
 
 func _ready() -> void:
-	Conductor.set_song(MISTA_GREEN_ROUGH_START, 81)#162)
+	for node in states:
+		node.hide()
+	set_crack_level(0)
+	Conductor.set_song(MISTA_GREEN_ROUGH_START, 81)
 	Conductor.play()
 	Conductor.connect("finished", _loop)
-	conducted_player.play("pulsing") 
+	conducted_player.play("pulsing")
 
 func _loop():
 	Conductor.set_song(MISTA_GREEN_ROUGH_LOOP, 81)
 	Conductor.play()
 
+func set_crack_level(level: int) -> void:
+	current_state = level
+	for i in states.size():
+		states[i].visible = (i == level)
+	shake()
+
 func _on_start_area_entered(area: Area2D) -> void:
-	count += 1
-	start.scale += Vector2(-.1, -.1)
-	if count == 3:
+	if current_state < states.size() - 1:
+		set_crack_level(current_state + 1)
+	else:
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func shake():
+	var tween = create_tween()
+	var rand_offset = Vector2(randi_range(-10, 10), randi_range(-10, 10))
+	position += rand_offset
+	tween.tween_property(self, "position", Vector2.ZERO, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
