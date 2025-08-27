d63c1d53e9221d55d7926835e1b90cb611d1337b
dibin_17 <dylan@rut66.net>
2025-08-27 15:24:18 -0600

    button collision

--- scripts/title.gd
+++ scripts/title.gd
@@ -4,6 +4,7 @@ const MISTA_GREEN_ROUGH_START = preload("res://assets/music/mista green rough st
 const MISTA_GREEN_ROUGH_LOOP = preload("res://assets/music/mista green rough loop.wav")
 
 @onready var conducted_player: ConductedAnimationPlayer = $ConductedAnimationPlayer
+@onready var explosion_sound: AudioStreamPlayer = $START/explosion
 
 @onready var states = [
 	$START/dam0,
@@ -22,6 +23,7 @@ func _ready() -> void:
 	Conductor.play()
 	Conductor.connect("finished", _loop)
 	conducted_player.play("pulsing")
+	explosion_sound.volume_db = -40
 
 func _loop():
 	Conductor.set_song(MISTA_GREEN_ROUGH_LOOP, 81)
@@ -36,6 +38,8 @@ func set_crack_level(level: int) -> void:
 func _on_start_area_entered(area: Area2D) -> void:
 	if current_state < states.size() - 1:
 		set_crack_level(current_state + 1)
+		explosion_sound.play()
+		explosion_sound.volume_db += 10
 	else:
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
 
