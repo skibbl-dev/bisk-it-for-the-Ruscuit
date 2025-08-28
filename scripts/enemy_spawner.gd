d1365249a676ef04356719b861282241f45b6df0
sᴋɪʙʙʟ_ <62254455+skibbl-dev@users.noreply.github.com>
2025-08-27 19:22:00 -0600

    slash animation + put peaberry font

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -7,6 +7,9 @@ var current_wave: Node2D
 @onready var wave_label: Label = $WaveLabel
 
 func spawn_next_wave():
+	if(!enemy_waves.has(wave)):
+		return # IF TEHRE ARE NO MORE WAVES, JUST GIVE UP>
+	
 	for child in Game.INSTANCE.bullet_container.get_children():
 		child.queue_free()
 	var new_wave = enemy_waves[wave].pick_random().instantiate()
