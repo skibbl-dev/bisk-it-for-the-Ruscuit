b9f54e5186cdb3aff9389bb1c6a3f635f13c0b94
dibin_17 <dylan@rut66.net>
2025-08-28 17:44:53 -0600

    death screen works

--- scripts/player.gd
+++ scripts/player.gd
@@ -131,7 +131,7 @@ func _on_hurtbox_area_entered(_area: Area2D) -> void:
 		return
 	last_hit = Conductor.current_beat
 	if sprite.frame == 3:
-		pass
+		get_tree().change_scene_to_file("res://scenes/death_screen.tscn")
 	else:
 		sprite.frame+=1
 	_area.queue_free()
