d0994ddfbaa6bcfb1d0633630e288b60f398094c
sᴋɪʙʙʟ_ <62254455+skibbl-dev@users.noreply.github.com>
2025-08-30 11:57:39 -0600

    fixed float to int warn

--- scripts/mouse_cursor.gd
+++ scripts/mouse_cursor.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 func _process(_delta: float) -> void:
-	var old_position:int = cursor.global_position.x
+	var old_position:float = cursor.global_position.x
 	cursor.global_position = get_global_mouse_position()
 	cursor.rotation = lerp_angle(cursor.rotation, deg_to_rad(remap((cursor.global_position.x - old_position), -max_movement_x, max_movement_x, -max_rotation_degrees, max_rotation_degrees)), rotation_lerp_weight)
 	
