0d0bfec053433ac6d9f040c4c231abefee588410
dibin_17 <dylan@rut66.net>
2025-08-30 14:35:16 -0600

    giggity death screen song

--- scenes/death_screen.gd
+++ scenes/death_screen.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+@onready var sad_music: AudioStreamPlayer = $SAD_MUSIC
+
+var can_again : bool = false
+
+func _ready() -> void:
+	Conductor.stop()
+	sad_music.play()
+	await get_tree().create_timer(2).timeout
+	can_again = true
+
+
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
-		get_tree().change_scene_to_file("res://scenes/game.tscn")
+		if can_again == true:
+			get_tree().change_scene_to_file("res://scenes/game.tscn")
